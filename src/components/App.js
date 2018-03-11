1364a3571d5c6138ddc5f3ab92610e5897039612
alexandrepol <alexandre.pol1996@gmail.com>
2018-03-11 18:56:38 +0100

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import '../styles/App.css';
+import LinkList from "./LinkList";
+import LinkListPage from "./LinkListPage";
+
+class App extends Component {
+  render() {
+    return (
+      <LinkListPage/>
+    );
+  }
+}
+
+export default App;
